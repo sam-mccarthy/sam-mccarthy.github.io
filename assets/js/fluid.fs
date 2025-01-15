ff8778cf7e0de51a1398c722a14597a2b061564b
sam-mccarthy <samuelmccarthy03@gmail.com>
2025-01-14 23:40:38 -0500

    Implemented advection

--- assets/js/fluid.fs
+++ assets/js/fluid.fs
@@ -1,3 +1,4 @@
+#version 300 es
 precision mediump float;
 
 uniform vec4 u_impulse;
@@ -8,7 +9,24 @@ uniform sampler2D u_vorticity;
 
 uniform vec2 u_resolution;
 
+vec4 f4texRECTbilerp(sampler2D tex, vec2 uv) {
+    vec2 weight = fract(uv);
+    vec4 bottom = mix(texture(tex, uv), 
+                        texture(tex, uv + vec2(1, 0)), 
+                        weight.x);
+    vec4 top = mix(texture(tex, uv + vec2(0, 1)), 
+                    texture(tex, uv + vec2(1, 1)), 
+                    weight.x);
+    
+    return mix(bottom, top, weight.y);
+}
+
+vec4 advect(vec2 coords, float timestep, float rdx, sampler2D u, sampler2D x) {
+    vec2 pos = coords - timestep * rdx * texture(u, coords).xy;
+    return f4texRECTbilerp(x, pos);
+}
+
 void main() {
     vec2 uv = vec2(gl_FragCoord) / u_resolution;
-    gl_FragColor = vec4(u_impulse.x, u_impulse.y, 0, 1);
+    
 }
