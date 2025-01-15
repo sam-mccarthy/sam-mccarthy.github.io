80d7d4809fc0e31a3e644c8b8c139f07a64f9925
sam-mccarthy <samuelmccarthy03@gmail.com>
2025-01-14 23:55:07 -0500

    Implementation of more steps in fluid sim

--- assets/js/fluid.fs
+++ assets/js/fluid.fs
@@ -26,7 +26,39 @@ vec4 advect(vec2 coords, float timestep, float rdx, sampler2D u, sampler2D x) {
     return f4texRECTbilerp(x, pos);
 }
 
+vec4 jacobi(vec2 coords, float alpha, float rBeta, sampler2D x, sampler2D b){
+    vec4 xL = texture(x, coords - vec2(1, 0));
+    vec4 xR = texture(x, coords + vec2(1, 0));
+    vec4 xB = texture(x, coords - vec2(0, 1));
+    vec4 xT = texture(x, coords + vec2(0, 1));
+    vec4 bC = texture(x, coords);
+
+    return (xL + xR + xB + xT + alpha * bC) * rBeta;
+}
+
+vec4 divergence(vec2 coords, float halfrdx, sampler2D w){
+    vec4 wL = texture(w, coords - vec2(1, 0));
+    vec4 wR = texture(w, coords + vec2(1, 0));
+    vec4 wB = texture(w, coords - vec2(0, 1));
+    vec4 wT = texture(w, coords + vec2(0, 1));
+
+    return vec4(halfrdx * ((wR.x - wL.x) + (wT.y - wB.y)), 0.0, 0.0, 1.0);
+}
+
+vec4 gradient(vec2 coords, float halfrdx, sampler2D p, sampler2D w){
+    float pL = texture(p, coords - vec2(1, 0)).x;
+    float pR = texture(p, coords + vec2(1, 0)).x;
+    float pB = texture(p, coords - vec2(0, 1)).x;
+    float pT = texture(p, coords + vec2(0, 1)).x;
+
+    return texture(w, coords) - halfrdx * vec4(0, 0, pR - pL, pT - pB);
+}
+
+vec4 boundary(vec2 coords, vec2 offset, float scale, sampler2D x){
+    return scale * texture(x, coords + offset);
+}
+
 void main() {
     vec2 uv = vec2(gl_FragCoord) / u_resolution;
-    
+
 }
